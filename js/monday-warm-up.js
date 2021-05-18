8d2701ec0c2adcdbf7bf62ed91d3d864a5a4509b
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-05-18 00:02:47 -0700

    adds js-monday-warm-up.js

---
+++ js/monday-warm-up.js
@@ -0,0 +1,5 @@
+"use strict";
+// Write a function named secondToLast(arr) that returns the data type of the second to last element of the array parameter. When you call the function, pass in an array of your choosing
+function secondToLast(arr) {
+
+}
